39a1fc3d6c7fb2f5439445cb8411e019a777d024
Oluwaremi Oluwole <polymathwole@gmail.com>
2019-01-13 22:45:12 +0100

    More Promises practice

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ fetchWeather = async () => {
     await fetch('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22')
     .then(response => response.json())
     .catch(err => console.log(err));
-    console.log(data);
-    console.log(data.coord);
+    console.log(data);  
 }
 
-fetchWeather();
+const saySomething = (whatToSay) => console.log(whatToSay);
+//setTimeout(() => saySomething("3 seconds passed"), 3000); // asynchronous
+//saySomething('Bleh!'); // synchronous
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+const failureCallback = () => console.log('Error!');
+wait(3000).then(() => saySomething("3 seconds passed")).catch(failureCallback);
